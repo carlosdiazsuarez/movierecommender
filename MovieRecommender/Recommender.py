c6f84eb43241b8ac437e620d465ef202ae087ddf
Jaime <Jaime@192.168.1.35>
2015-04-16 11:25:41 +0200

    working piece OMDB + twitter secure + twitter streaming working they retrieve info related to the stream given in initial variable (in this case 'Obama')

--- MovieRecommender/Recommender.py
+++ MovieRecommender/Recommender.py
@@ -1,7 +1,7 @@
 '''
 Created on 14/04/2015
 
-@author: cdiaz
+@author: cdiaz jandres
 '''
 
 import re, math
@@ -9,11 +9,11 @@ import json
 from collections import Counter
 
 from connectors.omdb.omdb_connector import omdb_connector
+
 from connectors.twitter.twitter_patternPkg_connector import twitter_patternPkg_connector
 from connectors.twitter.twitter_streaming_connector import twitter_streaming_connector
 
-if __name__ == '__main__':
-    main()
+
 
 
 def main():
@@ -25,28 +25,46 @@ def main():
     # THAT SHOULD NOT BE A PROBLEM SINCE THE TWEETER STUFF IS FOR ENRICHMENT OPERATIONS
     
     film_to_search = 'Obama'
-    print film_to_search
+    print 'TOPIC TO SEARCH: ' + film_to_search
+    print 
+    
     
     # SOURCE 1 OMDb Movie Details
+    print '\n' + '*'*40
+    print 'PROCEEDING TO OMBD search:'
+    print '*'*40
     omdb_conn = omdb_connector()
-    #OMBd_loader.load_Initial_Set_Movies()
+    #omdb_conn.load_Initial_Set_Movies()
     json_movie_result = omdb_conn.getMovie_by_Name(film_to_search)
-    #print json_movie_result
-    #print json.dumps(json_movie_result, indent=2)
+    print 'OMDb LOAD - PRINTING JSON file:'
+    print json_movie_result
+    print json.dumps(json_movie_result, indent=2)
     
     
     # SOURCE 4 A TWITTER SECURE LOAD
+    print '\n' + '*'*40
+    print 'PROCEEDING TO TWITTER SECURE SEARCH:'
+    print '*'*40
     twitter_secureLoad_conn = twitter_patternPkg_connector ()
     tweets_initialDump_List = twitter_secureLoad_conn.getTweetSecureLoad(film_to_search)
-    #print 'TWEETER SECURE LOAD - PRINTING LIST OF TWEETS (LIST OF [tweet.id, tweet.date, oneSubject, tweet.text]):'
-    #print tweets_initialDump_List
+    print 'TWEETER SECURE LOAD - PRINTING LIST OF TWEETS (LIST OF [tweet.id, tweet.date, oneSubject, tweet.text]):'
+    print tweets_initialDump_List
    
    
     # SOURCE 4 B TWITTER STREAMING LOAD
+    print '\n' + '*'*40
+    print 'PROCEEDING TO TWITTER STREAMING SEARCH:'
+    print '*'*40
     twitter_StreamingLive_conn = twitter_streaming_connector()
     tweets_Streaming_List = twitter_StreamingLive_conn.getTweetStreamingLive(film_to_search)
-    #print 'TWEETER STEAMING LOAD - PRINTING LIST OF TWEETS (LIST OF [tweet.id, tweet.date, oneSubject, tweet.text]):'
-    #print tweets_Streaming_List
+    print 'TWEETER STEAMING LOAD - PRINTING LIST OF TWEETS (LIST OF [tweet.id, tweet.date, oneSubject, tweet.text]):'
+    print tweets_Streaming_List
+
+
+    print 'FINISHED ALL SEARCHES: ' + film_to_search
+    
 
     ################################################## JAIME END 
 
+if __name__ == '__main__':
+    main()
